9df984429340406213e1ee23aa4619c91b56051f
velicharlagokulkumar <velicharlagokulkumar@gmai.com>
2023-01-05 10:41:34 +0530

    init

---
+++ chapter-5/5.2.1/exrand.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+//Gaussian random numbers
+gaussian("gau.dat", 1000000);
+return 0;
+}
+
+
+
