5d1a202ec9201f1c27c7a10f4ab4f925f1cfe9e6
velicharlagokulkumar <velicharlagokulkumar@gmai.com>
2023-01-06 05:12:31 +0530

    fine

---
+++ codes/chapter-5/5.4/uniform_two.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+uniform("uni1.dat", 1000000);
+uniform("uni2.dat", 1000000);
+return 0;
+}
